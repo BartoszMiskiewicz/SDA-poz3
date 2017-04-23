acb79b6984709a841f2958c40b4dd68479bf52e2
BartoszMiskiewicz <bartosz-miskiewicz@wp.pl>
2017-04-23 14:13:56 +0200

    Some file operations

---
+++ src/PierwszeZadanie/Eleven.java
@@ -0,0 +1,61 @@
+package PierwszeZadanie;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Created by bartosz on 21.04.2017.
+ */
+public class Eleven {
+    public static void main(String[] args) throws IOException {
+        File file = new File("/Volumes/Dane/JavaProgramowanie/file");
+        readFromFileTest(file);
+//        writeToFileTest(file, "Hello World");
+        readIntegerFromFile(file);
+    }
+
+    public static int[] readIntegerFromFile(File file) throws FileNotFoundException {
+        int[] array = new int[getLength(file)];
+        Scanner scanner = new Scanner(file);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static int countOddIntegers(File file){
+        return 0;
+    }
+
+
+    public static int getLength(File file) throws FileNotFoundException { //okresla wielkosc tabliczy
+        Scanner scanner = new Scanner(file);
+        int counter = 0;
+        while (scanner.hasNext()){
+            counter++;
+            scanner.nextLine();
+        }
+        return counter;
+    }
+
+    private static void readFromFileTest(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNext()) {// sprawdza nam jezeli jest nowa linia to zrob do
+            System.out.println(scanner.nextLine()); //wypisz wszystko z pliku hellotext
+        }
+    }
+
+    private static void writeToFileTest(File file, String message) throws IOException{
+        try (FileWriter fw = new FileWriter(file, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+            out.println(message);
+
+        }
+        System.out.println(message);
+        // try (ajdnaskdnaksdm){
+        //    asdasa
+        // }
+    }
+
+}
--- src/PierwszeZadanie/Ten.java
+++ src/PierwszeZadanie/Ten.java
@@ -50,6 +50,112 @@ public class Ten {
 
 
     }
+
+
+    //Ala ma kota, a -> 3
+    public static int countAll(String message, char sentence) {
+        char[] charArray = message.toCharArray();
+        int counter = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            if (toLowerCase(charArray[i]) == toLowerCase(sentence)){
+            counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static char toLowerCase(char a){
+        if (a >= 65 && a<= 90){
+            a += 32;
+        }
+        return a;
+
+    }
+
+
+
+    //aabbbbbccee -> {2, 5, 2, 0, 2, ...}
+    //Ala ma kota i borsuka -> {5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, ...}
+    public static int[] stringStatistics(String message) {
+        char[] charArray = message.toCharArray(); // ala ma kota messeage mamy
+        int [] ints = new int [26];//tworzymy tablice
+        for (int i = 0; i < charArray.length ; i++){
+            char charInLowerCase = toLowerCase(charArray[i]);//obnizamy do male litety i wtedy zliczamy jeje wysapienie
+            if(charInLowerCase >= 97 && charInLowerCase <= 122) {//sprawdzamy czy mamy małe litery w wyrazie czyli przedział od 97 do 122
+                ints[charInLowerCase - 97]++;
+            }
+
+        }
+
+        return ints;
+
+    }
+
+    //{2, 4, 2, 0, 2, ...} -> aabbbbbccee
+    public static void printStringStatistics(int[] array){
+// String message = "";
+        for (int i = 0; i < array.length; i++) {//bierze kazdy elemetn tablicy array i tworzymy petle numer dwa ktora
+            // bedzie sie wykonywala tyle razy ile mamy elementy
+            for (int j = 0; j < array[i]; j++) {
+                System.out.print((char)(i + 97));//zamienimy cyfry na literki spowrotem i+97 to daje nam litere c
+// message += (char) (i+ 97);
+            }
+
+        }
+// System.out.println(message);
+// return message;
+
+    }
+
+    //123 -> 1 * 2 * 3 = 6
+    public static int multiplyDigits(int number){
+        int product = 1;
+        while (number != 0){//robimy to tak dlugo dopuki number jest rowne lub rozne od zero
+            product *= number % 10;
+            number /= 10;
+        }
+        return product;
+    }
+
+    public static double avg(int[][] matrix){
+        int sum = sum(matrix);
+        return sum/(double)(matrix.length * matrix[0].length);
+    }
+
+    public static int sum(int[][] matrix){
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                sum += matrix[i][j];
+
+            }
+
+        }
+        return sum;
+    }
+
+    public static double [][] avg(int[][] matrix1, int[][] matrix2){
+        double[][] resultMatrix = new double[matrix1.length][matrix1[0].length];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[0].length; j++) {
+                resultMatrix[i][j] = (matrix1[i][j]+ matrix2[i][j]) / 2.0;
+            }
+        }
+        return resultMatrix;
+    }
+
+    public static int [][] randomMatrix(int size, int bounds, int offset){
+        Random random = new Random();
+        int[][] resultMatrix = new int [size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                resultMatrix[i][j] = random.nextInt(bounds) + offset;
+            }
+        }
+        return resultMatrix;
+
+    }
+
     //01234567
     //Ala ma kota, kota -> 7
     public  static  int find(String message, String sentence) {
@@ -57,7 +163,7 @@ public class Ten {
         char[] sentenceArray = sentence.toCharArray();
         boolean flag = true;
         int i = 0;
-        while(flag && i < messageArray.length - sentenceArray.length) {
+        while(flag && i <= messageArray.length - sentenceArray.length) {
             int j = 0;
             while (j < sentenceArray.length && messageArray[i+j] == sentenceArray[j]) {
                 j++;
@@ -69,32 +175,19 @@ public class Ten {
 
         }
         return flag ? -1 : i - 1;
-
     }
-
-    //Ala ma kota, a -> 3
-//    public static int countAll (String message, char sentence) {
-//        char[] charArray = message.toCharArray();
-//        int counter = 0;
-//        for (int i = 0; i < charArray.length; i++) {
-//            if (charArray[i] == sentence)}
-//            counter++;
-//            }
-//        }
-//        return counter++;
-//    }
 
     //Ala ma kota i ma tez psa, ma ->2
     public static int countAll(String message,String sentence) {
         int counter = 0;
         while (find(message, sentence)!= -1){
             counter++;
-            message = message.substring(find(message, sentence) +1 );
-
-
+            message = message.substring(find(message, sentence) +1);
         }
         return counter;
     }
+
+
     //((2+2)*2) -> true
     //(2+2)*2) -> false
     //)(2+2)*2( -> false
@@ -114,106 +207,21 @@ public class Ten {
         return counter == 0;
     }
 
-
-    public static char toLowerCase(char a) {
-        if (a >= 65 && a <= 90 ){
-            a += 32;
-        }
-        return a;
-
+    //DO domu
+    public static String toBinary(int number) {
+        return null;
     }
 
-
-
-
-    //aabbbbbccee -> {2, 5, 2, 0, 2, ...}
-    //Ala ma kota i borsuka -> {5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, ...}
-    public static int[] stringStatistics(String message) {
-        char[] charArray = message.toCharArray(); // ala ma kota messeage mamy
-        int [] ints = new int [26];//tworzymy tablice
-        for (int i = 0; i < charArray.length ; i++){
-            char charInLowerCase = toLowerCase(charArray[i]);//obnizamy do male litety i wtedy zliczamy jeje wysapienie
-            if(charInLowerCase >= 97 && charInLowerCase <= 122) {//sprawdzamy czy mamy małe litery w wyrazie czyli przedział od 97 do 122
-                ints[charInLowerCase-97]++;
-            }
-
-        }
-
-        return ints;
-
+    //DO domu
+    public static int toNumber(String binaryCode) {
+        return 0;
     }
 
-    //{2, 4, 2, 0, 2, ...} -> aabbbbbccee
-    public static void printStringStatistics(int[] array){
-
-        for (int i = 0; i < array.length; i++) {//bierze kazdy elemetn tablicy array i tworzymy petle numer dwa ktora
-            // bedzie sie wykonywala tyle razy ile mamy elementy
-            for (int j = 0; j < array[i]; j++) {
-                System.out.print((char)(i + 97));//zamienimy cyfry na literki spowrotem i+97 to daje nam litere c
-
-            }
-
-        }
-
-
-
+    //DO domu
+    //#FF0050 -> {255, 0, 80}
+    public static int[] rgbToValues(String rgb) {
+        return null;
     }
-    //123 -> 1 * 2 * 3 = 6
-    public static int multiplyDigits(int number){
-        int product = 1;
-        while (number != 0){//robimy to tak dlugo dopuki number jest rowne lub rozne od zero
-            product *= number % 10;
-            number /= 10;
-        }
-        return product;
-    }
-
-    public static int [][] randomMatrix(int size, int bounds, int offset){
-        Random random = new Random();
-        int[][] resultMatrix = new int [size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                resultMatrix[i][j] = random.nextInt(bounds) + offset;
-            }
-        }
-        return resultMatrix;
-
-    }
-
-
-//    public static int avg(int[][] matrix){
-//        int sum = sum(matrix);
-//        return sum/(double)(matrix.length * matrix[0].length);
-
-//
-//    }
-
-    public static int sum(int[][] matrix){
-        int sum = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                sum += matrix[i][j];
-
-            }
-
-        }
-        return sum;
-    }
-
-    public static double [][] avg(int[][] matrix1, int[][] matrix2){
-        double[][] resultMatrix = new double[matrix1.length][matrix1[0].length];
-        for (int i = 0; i < matrix1.length; i++) {
-            for (int j = 0; j < matrix1[0].length; j++) {
-                resultMatrix[i][j] = (matrix1[i][j]+ matrix2[i][j]) / 2.0;
-
-            }
-
-        }
-        return resultMatrix;
-    }
-
-
-
 
 
 }
